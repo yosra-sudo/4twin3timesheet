1de7a0088c877d51c1716d4224e5e9ddfce4bf78
youssra bacha <bacha.youssra@esprit.tn>
2024-02-26 10:34:10 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -17,7 +17,7 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
-
+//commentaire 4TWIN3
 
 	private static final long serialVersionUID = 1L;
 
